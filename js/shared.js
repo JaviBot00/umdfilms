90ba5f4026a995bf34f96d51200fae5bc1ac3966
HotGuy <javibot00@gmail.com>
2026-06-09 23:26:06 +0200

    [master] fix: restore 'umdfilms/' prefix in rootPath function for correct path resolution

--- js/shared.js
+++ js/shared.js
@@ -16,7 +16,7 @@ async function fetchJSON(path) {
 function rootPath(path) {
   // Detecta si estamos en una subcarpeta (equipo/, portafolio/, material/)
   const depth = window.location.pathname.split('/').filter(Boolean).length - 1;
-  const prefix = depth > 0 ? '../'.repeat(depth) : '';
+  const prefix = depth > 0 ? '../'.repeat(depth) : 'umdfilms/';
   // return prefix + 'umdfilms/' + path;
   return prefix + path;
 }
